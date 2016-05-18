7be5e75a52aed054cb019785d89198842d4ff53e
shunsuke227ono <8144677452@mail.ecc.u-tokyo.ac.jp>
2016-05-18 18:34:52 +0900

    add sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_companies
+
+  private
+
+  def set_companies
+    @companies = Company.all
+  end
 end
