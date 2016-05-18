00df76c850cb00abca9be07c39d9c3fbf1a590bd
shunsuke227ono <8144677452@mail.ecc.u-tokyo.ac.jp>
2016-05-18 14:52:54 +0900

    add company model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160518054907) do
+ActiveRecord::Schema.define(version: 20160518055143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160518054907) do
   create_table "comedians", force: :cascade do |t|
     t.string   "name",       null: false
     t.string   "img"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
