0bb1b1d226ac7824c4ff131ae44576aee751759e
shunsuke227ono <8144677452@mail.ecc.u-tokyo.ac.jp>
2016-05-27 15:49:39 +0900

    add default method to coappear

--- app/models/co_appear.rb
+++ app/models/co_appear.rb
@@ -1,2 +1,29 @@
 class CoAppear < ActiveRecord::Base
+  # NOTE: Ensure 'comedian_id_1 < comedian_id_2'
+  class << self
+    def find_pair(comedian_id_a, comedian_id_b)
+      find_by(
+        comedian_id_1: comedian_id_1(comedian_id_a, comedian_id_b),
+        comedian_id_2: comedian_id_2(comedian_id_a, comedian_id_b)
+      )
+    end
+
+    def create_pair(comedian_id_a, comedian_id_b)
+      create(
+        comedian_id_1: comedian_id_1(comedian_id_a, comedian_id_b),
+        comedian_id_2: comedian_id_2(comedian_id_a, comedian_id_b),
+        count: 1
+      )
+    end
+
+    private
+
+    def comedian_id_1(comedian_id_a, comedian_id_b)
+      [comedian_id_a, comedian_id_b].min
+    end
+
+    def comedian_id_2(comedian_id_a, comedian_id_b)
+      [comedian_id_a, comedian_id_b].max
+    end
+  end
 end
