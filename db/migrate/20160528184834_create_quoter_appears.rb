29fa0e9f83da47ceebf646486040b97a931ec630
shunsuke227ono <8144677452@mail.ecc.u-tokyo.ac.jp>
2016-05-29 03:50:20 +0900

    rake db:migrate

--- db/migrate/20160528184834_create_quoter_appears.rb
+++ db/migrate/20160528184834_create_quoter_appears.rb
@@ -1,7 +1,9 @@
 class CreateQuoterAppears < ActiveRecord::Migration
   def change
     create_table :quoter_appears do |t|
-
+      t.integer :count, default: 0
+      t.integer :comedian_id, null: false
+      t.date :start_date, null: false
       t.timestamps null: false
     end
   end
