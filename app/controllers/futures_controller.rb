2c449b54f6be8b089b703fc1a248239383f56c23
shunsuke227ono <8144677452@mail.ecc.u-tokyo.ac.jp>
2016-05-30 05:57:19 +0900

    fix kiesou

--- app/controllers/futures_controller.rb
+++ app/controllers/futures_controller.rb
@@ -2,5 +2,7 @@ class FuturesController < ApplicationController
   def index
     @growths = Growth.includes(:comedian).where(year: '2015/04/01').
       where('this_year_count < 100 AND last_year_count > 3').order(rate: :desc).limit(50)
+    @dying = Growth.includes(:comedian).where(year: '2015/04/01').
+      where('this_year_count < 30').order(rate: :asc).limit(50)
   end
 end
